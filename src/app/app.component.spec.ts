220cce4e4116acc0f93ad6a558748c6f8bf2a6c6
kenproxx <kienhue98@gmail.com>
2022-07-26 15:21:46 +0700

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,3 +33,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span')?.textContent).toContain('minitestFE app is running!');
   });
 });
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
